087ae336122351b65e597cfe37855cd5993e13d7
ambition_echo <ambition_echo@outlook.com>
2022-07-24 21:13:50 +0800

    fix path

--- src/trayicon.cpp
+++ src/trayicon.cpp
@@ -98,7 +98,7 @@ void TrayIcon::handle()
     qDebug() << "handling...";
     QString earthSource = settings->value("APP/earthSource").toString();
     QString earthSize = settings->value("APP/earthSize").toString();
-    QString command = "python " + earthSource + ".py " + QString::number(this->height) + " " +
+    QString command = "python scripts/" + earthSource + ".py " + QString::number(this->height) + " " +
                       QString::number(this->width) + " " + earthSize;
     // 根据设置下载、更新壁纸
     qDebug() << command;
