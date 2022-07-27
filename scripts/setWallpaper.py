5fd9ef68d883e815cf5e63123796e64a15b20017
Wolfson Liu <wolfsonliu@live.com>
2022-07-27 11:13:59 -0700

    add GNOME settings

--- scripts/setWallpaper.py
+++ scripts/setWallpaper.py
@@ -13,5 +13,11 @@ def set_wallpaper(file):
         print(file)
         dbus = f"qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {{d = allDesktops[i];d.wallpaperPlugin = \"org.kde.image\";d.currentConfigGroup = Array(\"Wallpaper\", \"org.kde.image\", \"General\");d.writeConfig(\"Image\", \"file://{file}\")}}'"
         os.system(dbus)
+    elif (DE == 'GNOME'):
+        print(file)
+        gs1 = "gsettings set org.gnome.desktop.background picture-uri {}".format(file)
+        gs2 = "gsettings set org.gnome.desktop.background picture-uri-dark {}".format(file)
+        os.system(gs1)
+        os.system(gs2)
     else:
         print("该桌面环境暂不支持")
