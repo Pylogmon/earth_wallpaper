d3527cc4ae60f7fed6936564e3cfb03075200734
ambition_echo <ambition_echo@outlook.com>
2022-07-28 10:59:07 +0800

    添加自动构建

---
+++ package/package.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+mkdir package_space
+cp -r ../debian ./package_space/DEBIAN
+cp ../build/earth_wallpaper ./package_space/opt/earth_wallpaper/earth_wallpaper
+cp -r ../build/scripts ./package_space/opt/earth_wallpaper/scripts
+cp -r ../build/template ./package_space/opt/earth_wallpaper/template
+cp ../resource/icon.png ./package_space/opt/earth_wallpaper/icon.png
+
+sed -i "s/Version: .*/Version: $(git describe --long 2>/dev/null | sed  's/\([^-]*-g\)/r\1/;s/-/./g')/g" package_space/DEBIAN/control && git describe --long 2>/dev/null || sed -i "s/Version: .*/Version: r$(git rev-list --count HEAD).$(git rev-parse --short HEAD)/g"  package_space/DEBIAN/control
+
+dpkg -b ./package_space earth-wallpaper-amd64.deb
