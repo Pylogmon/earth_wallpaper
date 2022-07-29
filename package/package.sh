ae3213dc8e2642053451367a873bba6fccaf090f
ambition_echo <ambition_echo@outlook.com>
2022-07-29 16:31:46 +0800

    添加版本号自动更新

--- package/package.sh
+++ package/package.sh
@@ -5,6 +5,7 @@ cp ../build/earth-wallpaper ./packageSpace/opt/apps/cn.huguoyang.earthwallpaper/
 cp -r ../build/scripts ./packageSpace/opt/apps/cn.huguoyang.earthwallpaper/files/bin/scripts
 cp -r ../build/template ./packageSpace/opt/apps/cn.huguoyang.earthwallpaper/files/bin/template
 
-sed -i "s/Version: .*/Version: $(git describe --long 2>/dev/null | sed  's/\([^-]*-g\)/r\1/;s/-/./g')/g" packageSpace/DEBIAN/control && git describe --long 2>/dev/null || sed -i "s/Version: .*/Version: r$(git rev-list --count HEAD).$(git rev-parse --short HEAD)/g"  packageSpace/DEBIAN/control
+sed -i "s/Version: .*/Version: $(git describe --tags |  sed  's/\([^-]*-g\)/r\1/;s/-/./g')/g" packageSpace/DEBIAN/control
+sed -i "s/\"version\": .*/\"version\": \"$(git describe --tags |  sed  's/\([^-]*-g\)/r\1/;s/-/./g')\",/g" packageSpace/opt/apps/cn.huguoyang.earthwallpaper/info
 
 dpkg -b ./packageSpace earth-wallpaper-amd64.deb
