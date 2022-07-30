b56c241d3e667867f5bca0aaae02836e2d9aeb69
ambition_echo <ambition_echo@outlook.com>
2022-07-30 12:14:11 +0800

    🎈 perf(package): 除deepin外其他发行版单独打包

--- package/package.sh
+++ package/package.sh
@@ -1,6 +1,5 @@
 #/bin/bash
 mkdir -p packageSpace/opt/apps/cn.huguoyang.earthwallpaper/files/bin
-cp -r ../debian ./packageSpace/DEBIAN
 cp ../build/earth-wallpaper ./packageSpace/opt/apps/cn.huguoyang.earthwallpaper/files/bin/earth-wallpaper
 cp -r ../build/scripts ./packageSpace/opt/apps/cn.huguoyang.earthwallpaper/files/bin/scripts
 cp -r ../build/template ./packageSpace/opt/apps/cn.huguoyang.earthwallpaper/files/bin/template
@@ -8,4 +7,8 @@ cp -r ../build/template ./packageSpace/opt/apps/cn.huguoyang.earthwallpaper/file
 sed -i "s/Version: .*/Version: $(git describe --tags |  sed  's/\([^-]*-g\)/r\1/;s/-/./g')/g" packageSpace/DEBIAN/control
 sed -i "s/\"version\": .*/\"version\": \"$(git describe --tags |  sed  's/\([^-]*-g\)/r\1/;s/-/./g')\",/g" packageSpace/opt/apps/cn.huguoyang.earthwallpaper/info
 
-dpkg -b ./packageSpace earth-wallpaper-amd64.deb
+dpkg -b ./packageSpace earth-wallpaper-deepin-amd64.deb
+
+cp -r ../DEBIAN/ ./packageSpace/
+chmod 755 ./packageSpace/DEBIAN/postinst
+dpkg -b ./packageSpace earth-wallpaper-other-amd64.deb
