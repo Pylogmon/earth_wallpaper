09475da13743b33e56d069aef0971ff04e0a65e6
ambition_echo <ambition_echo@outlook.com>
2022-08-08 21:22:51 +0800

    fix(scripts): 更换获取公网ip接口
    Author: ambition-echo

--- scripts/24hourWallpaper.py
+++ scripts/24hourWallpaper.py
@@ -27,7 +27,7 @@ def get_location():
     session = requests.Session()
     session.trust_env = False
     try:
-        ip = session.get('https://myip.ipip.net').text.split(" ")[1][3:]
+        ip = requests.get('https://checkip.amazonaws.com').text.strip()
         loc = requests.get("https://ipapi.co/{}/json/".format(ip)).json()
         latitude = float(loc["latitude"])
         longitude = float(loc["longitude"])
