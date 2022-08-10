487a9134926bc7ff79cc9f388efff5b70f753b8f
ambition_echo <ambition_echo@outlook.com>
2022-08-10 11:31:35 +0800

    chore(src): 修改线程输出
    Author: ambition-echo

--- src/thread.cpp
+++ src/thread.cpp
@@ -10,5 +10,13 @@ Thread::~Thread() = default;
 
 void Thread::run()
 {
-    qDebug() << system(this->command.toUtf8());
+    int ret = system(this->command.toUtf8());
+    if (ret == 0)
+    {
+        qDebug() << "脚本执行成功！";
+    }
+    else
+    {
+        qDebug() << "脚本执行失败，返回值 " << ret;
+    }
 }
