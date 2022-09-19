165464355dc4fb3e0c036792eef71516ef092518
ambition_echo <ambition_echo@outlook.com>
2022-09-19 11:02:28 +0800

    feat(scripts): 必应随机壁纸获取高清壁纸而不是1080p

--- scripts/Bing-rand.py
+++ scripts/Bing-rand.py
@@ -7,8 +7,10 @@ import datetime
 import os
 import shutil
 import sys
+import json
+import hashlib
 
-api_url = "https://bing.ioliu.cn/v1/rand"
+api_url = "https://bing.ioliu.cn/v1/rand?type=json"
 
 
 def download(path):
@@ -17,12 +19,31 @@ def download(path):
         "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0"
     }
     if (sys.argv[6] == "None"):
-        img = requests.get(api_url, headers=headers)
+        imgjson = requests.get(api_url, headers=headers)
     else:
         proxies = {"http": str(sys.argv[6]), "https": str(sys.argv[6])}
-        img = requests.get(api_url, headers=headers, proxies=proxies)
+        imgjson = requests.get(api_url, headers=headers, proxies=proxies)
+    img_url = json.loads(imgjson.content.decode())["data"]["url"]
+    img_url = img_url.replace("http://h1.ioliu.cn/bing/",
+                              "https://www.bing.com/th?id=OHR.")
+    img_url = img_url.replace("1920x1080", "UHD")
+    img_url = img_url[:-10]
+    print(img_url)
+
+    if (sys.argv[6] == "None"):
+        img = requests.get(img_url, headers=headers)
+    else:
+        proxies = {"http": str(sys.argv[6]), "https": str(sys.argv[6])}
+        img = requests.get(img_url, headers=headers, proxies=proxies)
     with open(path, "wb") as fwi:
         fwi.write(img.content)
+    with open(path, 'rb') as fp:
+        data = fp.read()
+    md5 = hashlib.md5(data).hexdigest()
+    if (md5 == "f0f7d2c575a576fcbe5904900906e27a"):
+        return False
+    else:
+        return True
 
 
 def main():
@@ -32,7 +53,9 @@ def main():
         shutil.rmtree(wallpaper_dir)
     os.makedirs(wallpaper_dir)
     name = wallpaper_dir + today.strftime("%Y%m%d%H%M%S") + ".png"  # windows S
-    download(name)
+    while (True):
+        if (download(name)):
+            break
     set_wallpaper(name)
 
 
