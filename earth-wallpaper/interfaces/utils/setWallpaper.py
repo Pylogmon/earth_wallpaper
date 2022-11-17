cd27ec7132e8779ba4523351681911dbd89bf127
ambition_echo <ambition_echo@outlook.com>
2022-11-17 08:59:48 +0800

    windows壁纸设置方式改为填充

--- earth-wallpaper/interfaces/utils/setWallpaper.py
+++ earth-wallpaper/interfaces/utils/setWallpaper.py
@@ -11,7 +11,7 @@ def set_wallpaper(file):
                                     "Control Panel\\Desktop", 0,
                                     win32con.KEY_SET_VALUE)
         win32api.RegSetValueEx(key, "WallpaperStyle", 0, win32con.REG_SZ,
-                               "2")  #2拉伸适应桌面，0桌面居中
+                               "10")  #2拉伸适应桌面，0桌面居中，10填充  拉伸会使一些图片变形
         win32api.RegSetValueEx(key, "TileWallpaper", 0, win32con.REG_SZ, "0")
         win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, file,
                                       win32con.SPIF_SENDWININICHANGE)
