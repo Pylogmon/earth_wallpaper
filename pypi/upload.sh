8632db88a2ecf174100e2e829bbdc13ee52c0236
辛巳流火 <ambition_echo@outlook.com>
2022-11-22 14:13:37 +0800

    fix(ci): 修复打包脚本导致的版本异常问题

--- pypi/upload.sh
+++ pypi/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed -i "s/return.*/return \"$(git describe --tags |  sed  's/\([^-]*-g\)/r\1/;s/-/./g')\"/g" earth_wallpaper/about.py
+sed -i "s/return \".*/return \"$(git describe --tags |  sed  's/\([^-]*-g\)/r\1/;s/-/./g')\"/g" earth_wallpaper/about.py
 python3 ./setup.py sdist bdist_wheel
 # twine upload ./dist/*
